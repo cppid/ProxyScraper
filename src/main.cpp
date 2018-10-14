4d636eb3fafc7c88c1cbbf6f8839bc766901b721
Ammar Faizi <ammarfaizi2@gmail.com>
2018-10-15 00:12:38 +0700

    Optimize script

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,33 @@
 
-#include <cstdio>
 
 #include "lib/TeaCurl.h"
 #include "lib/TeaPCRE.h"
 
 int main(int argc, char const *argv[])
 {
-	TeaCurl tch("https://free-proxy-list.net/");
-	tch.exec();
-	tch.close();
+	TeaCurl *tch = new TeaCurl("https://free-proxy-list.net/");
+	tch->exec();
+	tch->close();
+	size_t bodyLength = strlen(tch->getBody().c_str());
+	char *body = (char*)malloc(bodyLength);
 
-	char ***result = (char***)malloc(2000 * sizeof(char***));
-
-	TeaPCRE tre;
-	unsigned int i, j, matchCount, matchCountD2;
-
-	char *body = (char*)malloc(100000 * sizeof(char));
-	memcpy(body, tch.getBody().c_str(), strlen(tch.getBody().c_str()));
+	memcpy(body, tch->getBody().c_str(), bodyLength);
+	delete tch;
 
 	//
 	// <tr><td>74.204.10.10</td><td>59535</td><td>US</td><td class='hm'>United States</td><td>elite proxy</td><td class='hm'>no</td><td class='hx'>yes</td><td class='hm'>23 seconds ago</td></tr>
 	//
 	
-	tre.setPattern(
+	TeaPCRE *tre = new TeaPCRE;
+	tre->setPattern(
 		"(?:<tr><td>)([\\d\\.]*)(?:<.+<td>)(\\d{2,5})(?:<.+<td class='hm'>)(.*)(?:<)", 
 		PCRE_DOTALL | PCRE_CASELESS | PCRE_MULTILINE | PCRE_UNGREEDY
 	);
-	tre.setSubject(body);
-	tre.multiFindAll(result, &matchCount, &matchCountD2, 49);
+	tre->setSubject(body);
+	unsigned int i, j, matchCount, matchCountD2;
+	char ***result = (char***)malloc(2000 * sizeof(char***));
+	tre->multiFindAll(result, &matchCount, &matchCountD2, 49);
+	delete tre;
 
 	FILE *handle = fopen("result.txt", "w");
 	for (i = 0; i < matchCount; i++) {
