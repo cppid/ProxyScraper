64376844c88184d27a53faeea2a93ca7dc76b2c8
Ammar Faizi <ammarfaizi2@gmail.com>
2018-10-15 12:38:04 +0700

    Update TeaCurl.cpp

--- src/lib/TeaCurl.cpp
+++ src/lib/TeaCurl.cpp
@@ -1,3 +1,4 @@
+
 #include "TeaCurl.h"
 
 size_t TeaCurl::__teaCurlWriteFunc(void *contents, size_t size, size_t nmemb, void *userp) {
@@ -5,64 +6,64 @@ size_t TeaCurl::__teaCurlWriteFunc(void *contents, size_t size, size_t nmemb, vo
 	return size*nmemb;
 }
 
-TeaCurl::TeaCurl(std::string _url) {
-	url = _url;
-	ch = curl_easy_init();
+TeaCurl::TeaCurl(std::string url) {
+	this->url = url;
+	this->ch = curl_easy_init();
 	this->setUp();
 }
 
 CURL *TeaCurl::getCurlResource() {
-	return ch;
+	return this->ch;
 }
 
 void TeaCurl::exec() {
-	res = curl_easy_perform(ch);
-	if(res == CURLE_OK) {
-		curl_easy_getinfo(ch, CURLINFO_RESPONSE_CODE, &httpCode);
+	this->res = curl_easy_perform(ch);
+	if(this->res == CURLE_OK) {
+		curl_easy_getinfo(this->ch, CURLINFO_RESPONSE_CODE, &this->httpCode);
 	} else {
-		error = curl_easy_strerror(res);
+		this->error = curl_easy_strerror(this->res);
 	}
 }
 
 CURLcode TeaCurl::getInfo(CURLINFO infoOpt, void *ptr) {
-	return curl_easy_getinfo(ch, infoOpt, ptr);
+	return curl_easy_getinfo(this->ch, infoOpt, ptr);
 }
 
 CURLcode TeaCurl::getRes() {
-	return res;
+	return this->res;
 }
 
 std::string TeaCurl::getError() {
-	return error;
+	return this->error;
 }
 
 std::string TeaCurl::getResponseHeader() {
-	return responseHeader;
+	return this->responseHeader;
 }
 
 std::string TeaCurl::getBody() {
-	return out;
+	return this->out;
 }
 
 long TeaCurl::getHttpCode() {
-	return httpCode;
+	return this->httpCode;
 }
 
 void TeaCurl::setOpt(CURLoption opt, const void *val) {
-	curl_easy_setopt(ch, opt, val);
+	curl_easy_setopt(this->ch, opt, val);
 }
 
 void TeaCurl::setUp() {
-	curl_easy_setopt(ch, CURLOPT_URL, url.c_str());
-	curl_easy_setopt(ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_easy_setopt(ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_easy_setopt(ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_easy_setopt(this->ch, CURLOPT_URL, this->url.c_str());
+	curl_easy_setopt(this->ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_easy_setopt(this->ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_easy_setopt(this->ch, CURLOPT_FOLLOWLOCATION, 1);
 	
-	curl_easy_setopt(ch, CURLOPT_WRITEFUNCTION, TeaCurl::__teaCurlWriteFunc);	
-	curl_easy_setopt(ch, CURLOPT_WRITEDATA, &out);
+	curl_easy_setopt(this->ch, CURLOPT_WRITEFUNCTION, TeaCurl::__teaCurlWriteFunc);	
+	curl_easy_setopt(this->ch, CURLOPT_WRITEDATA, &this->out);
 
-	curl_easy_setopt(ch, CURLOPT_HEADERFUNCTION, TeaCurl::__teaCurlWriteFunc);
-	curl_easy_setopt(ch, CURLOPT_WRITEHEADER, &responseHeader);
+	curl_easy_setopt(this->ch, CURLOPT_HEADERFUNCTION, TeaCurl::__teaCurlWriteFunc);
+	curl_easy_setopt(this->ch, CURLOPT_WRITEHEADER, &this->responseHeader);
 }
 
 void TeaCurl::close() {
@@ -71,7 +72,7 @@ void TeaCurl::close() {
 }
 
 void TeaCurl::cleanUp() {
-	curl_easy_cleanup(ch);
+	curl_easy_cleanup(this->ch);
 }
 
 TeaCurl::~TeaCurl() {
